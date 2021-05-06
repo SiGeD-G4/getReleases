d8a75d34dfb28be62065f30ecabc191bd45b3c43
Gabriela Guedes <gabrielabguedes@gmail.com>
2021-05-06 00:41:08 -0300

    Add sonar fork variables
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- consts.js
+++ consts.js
@@ -1,11 +1,7 @@
 require('dotenv').config()
 const { REPO, TAG } = process.env
-const OWNER = 'fga-eps-mds';
-
-const repoSplit = REPO.split('-')
-const repoName = repoSplit[repoSplit.length - 1]
-
-const SONAR_ID = `fga-eps-mds_2020-2-G4-${repoName}`;
+const OWNER = 'SiGeD-G4';
+const SONAR_ID = `SiGeD-G4_${REPO}`;
 const SONAR_URL = `https://sonarcloud.io/api/measures/component_tree?component=${SONAR_ID}&metricKeys=files,functions,complexity,coverage,ncloc,comment_lines_density,duplicated_lines_density,security_rating,tests,test_success_density,test_execution_time,reliability_rating&ps=500`;
 
 module.exports = {
