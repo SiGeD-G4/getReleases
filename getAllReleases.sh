9f13c58916fbc4ea0463b4d0e1d494a1277d3f46
Gabriela <gabrielabguedes@gmail.com>
2021-05-08 00:38:18 +0000

    Update script
    Signed-off-by: Gabriela <gabrielabguedes@gmail.com>

--- getAllReleases.sh
+++ getAllReleases.sh
@@ -5,6 +5,39 @@ tag_frontend=(v0.1 v0.2 v0.3 v0.4 v0.5 v0.6 v0.7 v0.8 v0.9 v0.9.0 v0.10.0 v0.10.
 tag_sectors=(v0.1.0 v0.1.1 v1.0 v1.0.0 v1.0.1 v1.1.0 v1.1.1)
 tag_users=(v0.1 v0.2 v0.3 v0.3.0 v0.3.1 v0.3.2 v1.0.0 v1.0.1 v1.0.2)
 
+function copy_files_to_repo {
+  repo=$1
+  case $repo in
+    2020-2-SiGeD-Clients)
+      cp /root/clients-docker-compose.yml /root/2020-2-SiGeD-Clients/docker-compose.yml
+      cp /root/clients-package.json /root/2020-2-SiGeD-Clients/package.json
+      cp /root/clients.env /root/2020-2-SiGeD-Clients/.env
+    ;;
+    2020-2-SiGeD-Demands)
+      cp /root/demands-docker-compose.yml /root/2020-2-SiGeD-Demands/docker-compose.yml
+      cp /root/demands-package.json /root/2020-2-SiGeD-Demands/package.json
+      cp /root/demands.env /root/2020-2-SiGeD-Demands/.env
+    ;;
+    2020-2-SiGeD-Frontend)
+      cp /root/frontend-docker-compose.yml /root/2020-2-SiGeD-Frontend/docker-compose.yml
+      cp /root/frontend-package.json /root/2020-2-SiGeD-Frontend/package.json
+    ;;
+    2020-2-SiGeD-Sectors)
+      cp /root/sectors-docker-compose.yml /root/2020-2-SiGeD-Sectors/docker-compose.yml
+      cp /root/sectors-package.json /root/2020-2-SiGeD-Sectors/package.json
+      cp /root/sectors.env /root/2020-2-SiGeD-Sectors/.env
+    ;;
+    2020-2-SiGeD-Users)
+      cp /root/users-docker-compose.yml /root/2020-2-SiGeD-Users/docker-compose.yml
+      cp /root/users-package.json /root/2020-2-SiGeD-Users/package.json
+      cp /root/users.env /root/2020-2-SiGeD-Users/.env
+    ;;
+    *)
+      echo "REPO NOT FOUND"
+    ;;
+  esac
+}
+
 function run_sonar_on_tags {
   repo=$1
   tags=$@
@@ -20,7 +53,8 @@ function run_sonar_on_tags {
       echo $(date +'%m/%d/%Y %H:%M')
       echo $key;
       git reset $key
-      git add sonar-project.properties .github/workflows/sonarCoverage.yml
+      copy_files_to_repo $repo
+      git add sonar-project.properties .github/workflows/sonarCoverage.yml docker-compose.yml package.json .env
       git commit -m "Add sonar properties"
       git push -f
       sleep 5m
@@ -31,23 +65,25 @@ function run_sonar_on_tags {
   cd ..
 }
 
-#echo "======= GET RELEASES FOR CLIENTS ======="
-#cd 2020-2-SiGeD-Clients/
-#run_sonar_on_tags "2020-2-SiGeD-Clients" ${tag_clients[@]}
+echo "======= GET RELEASES FOR CLIENTS ======="
+cd 2020-2-SiGeD-Clients/
+run_sonar_on_tags "2020-2-SiGeD-Clients" ${tag_clients[@]}
 
-#echo "======= GET RELEASES FOR DEMANDS ======="
-#cd 2020-2-SiGeD-Demands/
-#run_sonar_on_tags  "2020-2-SiGeD-Demands" ${tag_demands[@]} 
+echo "======= GET RELEASES FOR DEMANDS ======="
+cd 2020-2-SiGeD-Demands/
+run_sonar_on_tags  "2020-2-SiGeD-Demands" ${tag_demands[@]} 
 
-#echo "======= GET RELEASES FOR FRONTEND ======="
-#cd 2020-2-SiGeD-Frontend/
-#run_sonar_on_tags  "2020-2-SiGeD-Frontend" ${tag_frontend[@]} 
+echo "======= GET RELEASES FOR FRONTEND ======="
+cd 2020-2-SiGeD-Frontend/
+run_sonar_on_tags  "2020-2-SiGeD-Frontend" ${tag_frontend[@]} 
 
-#echo "======= GET RELEASES FOR SECTORS ======="
+echo "======= GET RELEASES FOR SECTORS ======="
 cd 2020-2-SiGeD-Sectors/
 run_sonar_on_tags "2020-2-SiGeD-Sectors" ${tag_sectors[@]}
 
-#echo "======= GET RELEASES FOR USERS ======="
-#cd 2020-2-SiGeD-Users/
-#run_sonar_on_tags "2020-2-SiGeD-Users" ${tag_users[@]}
+echo "======= GET RELEASES FOR USERS ======="
+cd 2020-2-SiGeD-Users/
+run_sonar_on_tags "2020-2-SiGeD-Users" ${tag_users[@]}
+
+echo "======= FINISHED SCRIPT ======="
 
